2c47c3c1f5b0db7e1a8088d39e3f099308c8a97b
Andrey Churkin <andrey.churkin@devexpress.com>
2017-01-11 11:50:46 +0300

    Refactoring

--- TestCafe.py
+++ TestCafe.py
@@ -27,7 +27,7 @@ class AsyncProcess(object):
         self.killed = False
         startupinfo = None
 
-        if os.name == "nt":
+        if os.name == 'nt':
             startupinfo = subprocess.STARTUPINFO()
             startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 
@@ -35,22 +35,22 @@ class AsyncProcess(object):
         for k, v in proc_env.items():
             proc_env[k] = os.path.expandvars(v)
 
-        if sys.platform == "win32":
+        if sys.platform == 'win32':
             self.proc = subprocess.Popen(cmd,
                                          stdout=subprocess.PIPE,
                                          stderr=subprocess.PIPE,
                                          stdin=subprocess.PIPE,
                                          startupinfo=startupinfo,
                                          env=proc_env, shell=True)
-        elif sys.platform == "darwin":
-            self.proc = subprocess.Popen(["/bin/bash", "-l", "-c", cmd],
+        elif sys.platform == 'darwin':
+            self.proc = subprocess.Popen(['/bin/bash', '-l', '-c', cmd],
                                          stdout=subprocess.PIPE,
                                          stderr=subprocess.PIPE,
                                          stdin=subprocess.PIPE,
                                          startupinfo=startupinfo,
                                          env=proc_env, shell=False)
-        elif sys.platform == "linux":
-            self.proc = subprocess.Popen(["/bin/bash", "-c", cmd],
+        elif sys.platform == 'linux':
+            self.proc = subprocess.Popen(['/bin/bash', '-c', cmd],
                                          stdout=subprocess.PIPE,
                                          stderr=subprocess.PIPE,
                                          stdin=subprocess.PIPE,
@@ -73,10 +73,10 @@ class AsyncProcess(object):
     def kill(self):
         if not self.killed:
             self.killed = True
-            if sys.platform == "win32":
+            if sys.platform == 'win32':
                 startupinfo = subprocess.STARTUPINFO()
                 startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-                subprocess.Popen("taskkill /PID " + str(self.proc.pid),
+                subprocess.Popen('taskkill /PID ' + str(self.proc.pid),
                                  startupinfo=startupinfo)
             else:
                 self.proc.terminate()
@@ -111,10 +111,28 @@ class TestCafeCommand(sublime_plugin.WindowCommand):
     text_queue_proc = None
     text_queue_lock = threading.Lock()
 
-    previousCmd = None
+    previous_cmd = None
     proc = None
 
-    def run(self, cmd=None, browser=None, **kwargs):
+    def get_testcafe_cmd(self, test_code, file_name):
+        match = None
+        for match in re.finditer(FIND_TEST_OR_FIXTURE_RE, test_code, re.IGNORECASE | re.MULTILINE):
+            pass
+
+        testcafe_cmd = file_name
+
+        if match is not None:
+            fixture_name = match.group(2)
+            test_name = match.group(4)
+            if fixture_name is not None:
+                fixture_name = re.sub(CLEANUP_TEST_OR_FIXTURE_NAME_RE, '', fixture_name)
+                return testcafe_cmd + ' -f ' + '"' + fixture_name + '"'
+            elif test_name is not None:
+                test_name = re.sub(CLEANUP_TEST_OR_FIXTURE_NAME_RE, '', test_name)
+                return testcafe_cmd + ' -t ' + '"' + test_name + '"'
+        return testcafe_cmd
+
+    def run(self, cmd=None, browser=None):
         global BROWSER_LIST
         selection = self.window.active_view().sel()[0]
         file_name = self.window.active_view().file_name()
@@ -124,32 +142,16 @@ class TestCafeCommand(sublime_plugin.WindowCommand):
         if browser is None:
             browser = BROWSER_LIST[0]
 
-        testcafe_command = 'testcafe ' + browser + ' '
+        testcafe_cmd = 'testcafe ' + browser + ' '
 
-        if cmd == 'previous' and self.previousCmd is not None:
-            testcafe_command = self.previousCmd
+        if cmd == 'previous' and self.previous_cmd is not None:
+            testcafe_cmd = self.previous_cmd
         if cmd == 'all':
-            testcafe_command = testcafe_command + file_name
+            testcafe_cmd = testcafe_cmd + file_name
         else:
-            match = None
-            for match in re.finditer(FIND_TEST_OR_FIXTURE_RE, cursor_text, re.IGNORECASE | re.MULTILINE):
-                pass
+            testcafe_cmd = testcafe_cmd + self.get_testcafe_cmd(cursor_text, file_name)
 
-            if match is not None:
-                fixture_name = match.group(2)
-                test_name = match.group(4)
-                if fixture_name is not None:
-                    fixture_name = re.sub(CLEANUP_TEST_OR_FIXTURE_NAME_RE, '', fixture_name)
-                    testcafe_command = testcafe_command + file_name + ' -f ' + '"' + fixture_name + '"'
-                elif test_name is not None:
-                    test_name = re.sub(CLEANUP_TEST_OR_FIXTURE_NAME_RE, '', test_name)
-                    testcafe_command = testcafe_command + file_name + ' -t ' + '"' + test_name + '"'
-                else:
-                    testcafe_command = testcafe_command + file_name
-            else:
-                testcafe_command = testcafe_command + file_name
-
-        self.previousCmd = testcafe_command[:]
+        self.previous_cmd = testcafe_cmd[:]
         self.text_queue_lock.acquire()
         try:
             self.text_queue.clear()
@@ -158,22 +160,22 @@ class TestCafeCommand(sublime_plugin.WindowCommand):
             self.text_queue_lock.release()
 
         if not hasattr(self, 'output_view'):
-            self.output_view = self.window.create_output_panel("exec")
+            self.output_view = self.window.create_output_panel('exec')
 
-        self.output_view.settings().set("line_numbers", False)
-        self.output_view.settings().set("gutter", False)
-        self.output_view.settings().set("scroll_past_end", False)
-        self.window.create_output_panel("exec")
+        self.output_view.settings().set('line_numbers', False)
+        self.output_view.settings().set('gutter', False)
+        self.output_view.settings().set('scroll_past_end', False)
+        self.window.create_output_panel('exec')
 
         self.proc = None
 
-        print("Running testcafe: " + testcafe_command)
+        print('Running testcafe: ' + testcafe_cmd)
 
-        show_panel_on_build = sublime.load_settings("Preferences.sublime-settings").get("show_panel_on_build", True)
+        show_panel_on_build = sublime.load_settings('Preferences.sublime-settings').get('show_panel_on_build', True)
         if show_panel_on_build:
-            self.window.run_command("show_panel", {"panel": "output.exec"})
+            self.window.run_command('show_panel', {'panel': 'output.exec'})
 
-        self.proc = AsyncProcess(testcafe_command, self)
+        self.proc = AsyncProcess(testcafe_cmd, self)
 
         self.text_queue_lock.acquire()
         try:
@@ -201,7 +203,7 @@ class TestCafeCommand(sublime_plugin.WindowCommand):
 
             if len(self.text_queue) == 0:
                 was_empty = True
-                self.text_queue.append("")
+                self.text_queue.append('')
 
             available = self.BLOCK_SIZE - len(self.text_queue[-1])
 
@@ -220,7 +222,6 @@ class TestCafeCommand(sublime_plugin.WindowCommand):
     def service_text_queue(self):
         self.text_queue_lock.acquire()
 
-        is_empty = False
         try:
             if len(self.text_queue) == 0:
                 return
@@ -230,44 +231,35 @@ class TestCafeCommand(sublime_plugin.WindowCommand):
         finally:
             self.text_queue_lock.release()
 
-        self.output_view.run_command('append', {'characters': str, 'force': True, 'scroll_to_end': True})
+        self.output_view.run_command('append', {
+            'characters': str,
+            'force': True,
+            'scroll_to_end': True })
 
         if not is_empty:
             sublime.set_timeout(self.service_text_queue, 1)
 
 
-def add_items_to_context_menu():
-    template = json.loads(CONTEXT_MENU_TEMPLATE)
+def add_items_to_context_menu(template, file_name, cmd=None):
     testcafe_item = template[0]
     run_previous_item = testcafe_item['children'][0]
     separator_item = testcafe_item['children'][1]
     items = [run_previous_item, separator_item]
 
     for browser in BROWSER_LIST:
-        item = '{ "command": "test_cafe", "caption": "Run in ' + browser + '", "args": { "browser": "' + browser + '" }}'
-        items.append(json.loads(item))
+        item = { 'command': 'test_cafe',
+                 'caption': 'Run in ' + browser,
+                 'args': { 'browser': browser }}
+        if cmd is not None:
+            item['args']['cmd'] = cmd
+        items.append(item)
 
     testcafe_item['children'] = items
-    f = open(PACKAGE_PATH + '\\' + CONTEXT_MENU_FILE_NAME, 'w')
+    f = open(PACKAGE_PATH + '\\' + file_name, 'w')
     f.write(json.dumps(template))
     f.close()
 
+menu_template = json.loads(CONTEXT_MENU_TEMPLATE)
 
-def add_items_to_side_bar_menu():
-    template = json.loads(CONTEXT_MENU_TEMPLATE)
-    testcafe_item = template[0]
-    run_previous_item = testcafe_item['children'][0]
-    separator_item = testcafe_item['children'][1]
-    items = [run_previous_item, separator_item]
-
-    for browser in BROWSER_LIST:
-        item = '{ "command": "test_cafe", "caption": "Run in ' + browser + '", "args": { "cmd": "all", "browser": "' + browser + '" }}'
-        items.append(json.loads(item))
-
-    testcafe_item['children'] = items
-    f = open(PACKAGE_PATH + '\\' + SIDE_BAR_FILE_NAME, 'w')
-    f.write(json.dumps(template))
-    f.close()
-
-add_items_to_context_menu()
-add_items_to_side_bar_menu()
+add_items_to_context_menu(menu_template, CONTEXT_MENU_FILE_NAME)
+add_items_to_context_menu(menu_template, SIDE_BAR_FILE_NAME, 'all')
